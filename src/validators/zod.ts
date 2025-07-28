54b970cc03aa7f16c51352c251316f0e2375f406
Swastik Bhattacharyya <swastikunity@gmail.com>
2025-07-28 17:32:07 +0530

    docs: add documentation with jsdoc and typedoc
    Add documentation for the package using jsdoc and generate a
    documentation page using typedoc.
    
    Update cliff parser in config for docs commits.

--- src/validators/zod.ts
+++ src/validators/zod.ts
@@ -2,14 +2,25 @@ import z, { safeParse } from "zod";
 import { $ZodIssue, $ZodType } from "zod/v4/core";
 import { ValidationResultWithoutContext } from "../types";
 
-type ZodValidatorError = {
-  zodValidator_invalidParams: { issues: $ZodIssue[] };
-};
-
+/**
+ * A validator that checks input against a given Zod schema.
+ *
+ * If parsing fails, it returns a validation result containing the list of
+ * Zod issues under the error code `"zodValidator_invalidParams"`.
+ *
+ * @template Schema - The Zod schema used to validate the input.
+ * @param schema - The Zod schema instance.
+ * @param params - The parameters to validate.
+ * @returns A promise resolving to a ValidationResult. If parsing fails then
+ * the result contains the error code `"zodValidator_invalidParams` along with
+ * the list of Zod issues.
+ */
 export async function zodValidator<Schema extends $ZodType>(
   schema: Schema,
   params: z.infer<Schema>,
-): ValidationResultWithoutContext<ZodValidatorError> {
+): ValidationResultWithoutContext<{
+  zodValidator_invalidParams: { issues: $ZodIssue[] };
+}> {
   const result = safeParse(schema, params);
   if (!result.success)
     return {
